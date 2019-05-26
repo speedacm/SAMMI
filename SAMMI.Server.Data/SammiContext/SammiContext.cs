2c3e6172dab2896cf23ace1939e23d7917c08bde
Luke Caswell Samuel <luke.caswell.samuel@gmail.com>
2019-05-26 13:27:00 -0400

    Created solutions for SAMMI

---
+++ SAMMI.Server.Data/SammiContext/SammiContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SAMMI.Server.Data.SammiContext
+{
+	internal class SammiContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			// TODO: Set up a database for SAMMI, store connection string in seperate file that is *not* to be included on GitHub
+			optionsBuilder.UseSqlServer("");
+		}
+	}
+}
